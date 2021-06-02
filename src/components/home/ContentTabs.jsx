bd0b6be65eea03084b84b72e35f9e23c270a3225
ylu <ylu@ert.com>
2021-06-01 21:53:30 -0400

    refactor

--- src/components/home/ContentTabs.jsx
+++ src/components/home/ContentTabs.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 
+const tabObjs = [
+  {
+    icon: 'fa-door-open',
+    text: 'Cancel at any time',
+  },
+  {
+    icon: 'fa-tablet-alt',
+    text: 'Watch anywhere',
+  },
+  {
+    icon: 'fa-tags',
+    text: 'Pick your price',
+  },
+];
+
 const ContentTabs = ({ tabNum, setTabNum }) => {
   console.log(tabNum);
   return (
     <section className="tabs">
       <div className="container">
-        <div
-          className={tabNum === 1 && 'tab-border'}
-          onClick={() => setTabNum(1)}
-        >
-          <i className="fas fa-door-open fa-3x"></i>
-          <p className="hide-sm">Cancel at any time</p>
-        </div>
-        <div
-          className={tabNum === 2 && 'tab-border'}
-          onClick={() => setTabNum(2)}
-        >
-          <i className="fas fa-tablet-alt fa-3x"></i>
-          <p className="hide-sm">Watch anywhere</p>
-        </div>
-        <div
-          className={tabNum === 3 && 'tab-border'}
-          onClick={() => setTabNum(3)}
-        >
-          <i className="fas fa-tags fa-3x"></i>
-          <p className="hide-sm">Pick your price</p>
-        </div>
+        {tabObjs.map((tabObj, i) => (
+          <div
+            className={tabNum === i + 1 && 'tab-border'}
+            onClick={() => setTabNum(i + 1)}
+          >
+            <i className={`fas fa-3x ${tabObj.icon}`}></i>
+            <p className="hide-sm">{tabObj.text}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// https://www.youtube.com/watch?v=P7t13SGytRk&t=414s
+// Build a Netflix Landing Page Clone with HTML, CSS & JS May 21, 2019
