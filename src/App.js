8c640ed2fa9296c9e1cdb0cd94dace808b302ed1
ylu <ylu@ert.com>
2021-06-26 11:08:16 -0400

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import ContentTabs from './components/contents/ContentTabs';
+import Contents from './components/contents/Contents';
 import Footer from './components/Footer';
-import Home from './pages/home';
 
 function App() {
+  const [tabNum, setTabNum] = useState(1);
+
   return (
     <>
-      <Home />
+      <header className="showcase">
+        <Navbar />
+        <Banner />
+      </header>
+
+      <ContentTabs tabNum={tabNum} setTabNum={setTabNum} />
+      <Contents tabNum={tabNum} />
 
       <Footer />
     </>
